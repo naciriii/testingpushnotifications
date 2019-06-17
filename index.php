c59e446ba443265ed52079a15bdcf7cb6a7da14a
Nsiri abdennacer <nnacir1938@hotmail.com>
2019-06-17 12:54:55 +0100

    prettify

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo "\n___ payload json raw___ ". PHP_EOL;
 print($input);
 print($data);
 $decoded = json_decode($data);
-file_put_contents('payload.json',$input."\n"."Incomming message decoded: ".base64_decode($decoded->message->data), FILE_APPEND);
+file_put_contents('payload.json',$input."\n"."Incomming message decoded: ".base64_decode($decoded->message->data));
 file_put_contents('request.json',$data."\n", FILE_APPEND);
 echo "\n___ payload json parsed__ ". PHP_EOL;
 
