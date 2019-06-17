cf484c266461a427aaf0e8e81801dc8096fc9ecf
Nsiri abdennacer <nnacir1938@hotmail.com>
2019-06-17 10:38:24 +0100

    receive payload

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 var_dump($_REQUEST);
 $data = json_encode($_REQUEST);
+$input = file_get_contents('php://input');
+fle_put_contents('data1.json',$input);
 file_put_contents('data.json',$data);
 
