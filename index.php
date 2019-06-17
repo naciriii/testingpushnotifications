86f77471bdfd35ecebd55d074081b77b8747ac01
Nsiri abdennacer <nnacir1938@hotmail.com>
2019-06-17 13:02:42 +0100

    fuinal fix

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
 <?php
 
-$data = json_encode($_REQUEST);
 $input = file_get_contents('php://input');
-echo "\n___ payload json raw___ ". PHP_EOL;
-print($input);
-print($data);
 $decoded = json_decode($data);
 file_put_contents('payload.json',$input."\n"."Incomming message decoded: ".base64_decode($decoded->message->data));
 file_put_contents('request.json',serialize($decoded)."\n");
-echo "\n___ payload json parsed__ ". PHP_EOL;
-
-print_r(json_decode($input));
+die(200);
