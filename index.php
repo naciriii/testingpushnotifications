9f6e63faf73306eff46ca258fe131115d29a420f
Nsiri abdennacer <nnacir1938@hotmail.com>
2019-06-17 12:17:47 +0100

    print request

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $data = json_encode($_REQUEST);
 $input = file_get_contents('php://input');
 echo "\n___ payload json raw___ ". PHP_EOL;
 print($input);
+print($data);
 file_put_contents('payload.json',$input."\n", FILE_APPEND);
 file_put_contents('request.json',$data."\n", FILE_APPEND);
 echo "\n___ payload json parsed__ ". PHP_EOL;
