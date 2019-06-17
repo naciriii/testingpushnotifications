335a192ab504b36258046952914db4ff5da5c9a2
Nsiri abdennacer <nnacir1938@hotmail.com>
2019-06-17 12:57:34 +0100

    dump decoded

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ print($input);
 print($data);
 $decoded = json_decode($data);
 file_put_contents('payload.json',$input."\n"."Incomming message decoded: ".base64_decode($decoded->message->data));
-file_put_contents('request.json',$data."\n", FILE_APPEND);
+file_put_contents('request.json',var_dump($decoded)."\n");
 echo "\n___ payload json parsed__ ". PHP_EOL;
 
 print_r(json_decode($input));
