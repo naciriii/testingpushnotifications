212a757b8c36983005c549b717a1cbad9900f508
Nsiri abdennacer <nnacir1938@hotmail.com>
2019-06-17 13:05:14 +0100

    fix

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $input = file_get_contents('php://input');
-$decoded = json_decode($data);
+$decoded = json_decode($input);
 file_put_contents('payload.json',$input."\n"."Incomming message decoded: ".base64_decode($decoded->message->data));
 file_put_contents('request.json',serialize($decoded)."\n");
 die(200);
